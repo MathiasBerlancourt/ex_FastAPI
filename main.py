949b02d93ad3a3a6a2adbb72eee000b8b012ded4
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-07-07 00:38:37 +0200

    issue on post_question

--- main.py
+++ main.py
@@ -57,3 +57,32 @@ def get_quizz(use: str, subject: str, username: str = Header(...), password: str
         return quizz_selected
     else:
         return {"status": "error", "message": "mauvais username ou mot de passe"}
+
+
+@api.post("/question")
+def post_question(
+    question: str,
+    subject: str,
+    use: str,
+    responseA: str,
+    responseB: str,
+    responseC: str,
+    responseD: str,
+    remark: str,
+    username: str = Header(...),
+    password: str = Header(...),
+):
+    """post_question permet d'ajouter une question à la bdd. Un compte admin est nécessaire. Les clés subject, use, question, response A...D et remark doivent être complétées. On peut mettre un champ vide.
+    exemple:
+curl -X POST "http://localhost:8000/question" -H "username: admin" -H "password: 4dm1N" -H "subject: BDD" -H "use: Test%20de%20positionnement" -H "question: Quelle%20est%20la%20réponse%20de%20test%20?" -H "responseA: answer%20de%20test42" -H "responseB: answer%20de%20test42" -H "responseC: answer%20de%20test42" -H "responseD: answer%20de%20test42" -H "remark: test42"
+    """
+    if isAdmin(username, password):
+        with open("assets/questions.csv", "a") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(
+                [question, responseA, responseB, responseC,
+                    responseD, remark, subject, use]
+            )
+        return {"status": "success", "message": "Question ajoutée avec succès."}
+    else:
+        return {"status": "error", "message": "Mauvais nom d'utilisateur ou mot de passe."}
