0d3020239d9d78cc56a8959849ef1b0346b6b975
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-07-07 00:20:26 +0200

    add of get quizz with randomization

--- authentification.py
+++ authentification.py
@@ -4,9 +4,9 @@ with open('assets/users_db.json', 'r') as file:
     users_db = json.load(file)
 
 
-def isAuthentificated(username, password):
-    for user in users_db:
-        if user["username"] == username and user["password"] == password:
+def isAuthenticated(username, password):
+    for user, user_password in users_db.items():
+        if user == username and user_password == password:
             return True
     return False
 
@@ -14,5 +14,4 @@ def isAuthentificated(username, password):
 def isAdmin(username, password):
     if username == "admin" and password == "4dm1N":
         return True
-    else:
-        return False
+    return False
--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-from fastapi import FastAPI
-from isAuthentificated import isAuthentificated, isAdmin
+from fastapi import FastAPI, Header
+from authentification import isAuthenticated, isAdmin
 import csv
-import json
-
-with open('assets/users_db.json', 'r') as file:
-    users_db = json.load(file)
-
-print(users_db)
+import random
 
 
 api = FastAPI(title="API QUIZZ")
 
 
-def get_quizz():
-    """get quizz nous permet de lire notre fichier csv
-     et nous renvoie notre csv sous forme de dictionnaire """
+@api.get("/data")
+def get_data(username: str = Header(...), password: str = Header(...)):
+    """get_data nous permet de lire notre fichier csv
+     et nous renvoie notre csv sous forme de dictionnaire
+     on doit indiquer son username et son mdp pour accéder à la requête
+     exemple:
+     curl -X GET "http://localhost:8000/data" -H "username: alice" -H "password: wonderland"
+
+    """
     data = []
-    with open('assets/questions.csv', 'r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            data.append(row)
+    if isAuthenticated(username, password):
+        with open('assets/questions.csv', 'r') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                data.append(row)
 
-    # Retourne le contenu du fichier csv sous forme de dictionnaire
+        # Retourne le contenu du fichier csv sous forme de dictionnaire
+        return data
 
-    return data
-
-
-quizz_data = get_quizz()
+    else:
+        return {"status": "error", "message": "mauvais username ou mot de passe"}
 
 
 @api.get("/APIstatus")
@@ -34,3 +35,25 @@ def APIstatus():
     """APIstatus nous permet de savoir si l'API est en marche"""
     return {"status": "success",
             "message": "l'API est en marche"}
+
+
+@api.get("/quizz")
+def get_quizz(use: str, subject: str, username: str = Header(...), password: str = Header(...)):
+    """qet_quizz nous permet d'avoir un quizz généré aléatoirement en fonction 
+    du sujet (subject) et de l'utilisation (use) choisie. L'utilisateur doit s'authentifier pour accéder à ce quizz
+    exemple:
+     curl -X GET "http://localhost:8000/quizz?use=Test%20de%20positionnement&subject=BDD" -H "username: alice" -H "password: wonderland"
+    """
+    quizz_selected = []
+    if isAuthenticated(username, password):
+        with open('assets/questions.csv', 'r') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                if row['subject'] == subject and row['use'] == use:
+                    quizz_selected.append(row)
+
+        # je "mélange" mon quizz aléatoirement avec la fonction shuffle du paskage random :
+        random.shuffle(quizz_selected)
+        return quizz_selected
+    else:
+        return {"status": "error", "message": "mauvais username ou mot de passe"}
